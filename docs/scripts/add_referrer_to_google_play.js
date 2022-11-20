da40a0aa21a3290ba8215c0efce0224b4aec97ea
Artem IG <ortemeo@gmail.com>
2022-11-20 07:36:52 +0300

    update

--- docs/scripts/add_referrer_to_google_play.js
+++ docs/scripts/add_referrer_to_google_play.js
@@ -1,13 +1,21 @@
 function updateGooglePlayLinksNew() {
-    // меняем ссылки на Google Play. Во всех ссылках страница, которые ведут
-    // на https://play.google.com, добавляем параметр referrer, который содержит
-    // ссылку на собственно исходную страницу
+    // В текущей странице находим все A с параметром href, ведущим на
+    // https://play.google.com. Во всех этих ссылках исправляем href, добавляя
+    // к нему параметр referrer с адресом текущей страницы.
+    //
+    // То есть, если
+    //   страница https://revercode.com/app1 содержала
+    //   ссылку "https://play.google.com/?id=app1"
+    //   то ссылка будет заменена на
+    //   "https://play.google.com/?id=app1&referrer=https://revercode.com/app1"
+    // если адрес текущей страницы содержит query-строку, она конечно тоже
+    // будет закодирована и передана внутрь параметра referrer.
 
     function transformGooglePlayLinks(url2url) {
         for (var elem of document.getElementsByTagName("a")) {
             let href = elem.getAttribute("href");
             if (href!=null && href.startsWith("https://play.google.com")) {
-                elem.setAttribute( "href", url2url(elem.getAttribute("href")) );
+                elem.setAttribute("href", url2url(elem.getAttribute("href")));
             }
         }
     }
@@ -18,7 +26,6 @@ function updateGooglePlayLinksNew() {
         return u;
     };
 
-    //////////////
     window.onload = function () {
          transformGooglePlayLinks(url2url);
     };
